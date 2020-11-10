e00a2f26c7e21be2588bd3633b42b1b4bb88dbcf
iamogbz <iamogbz+github@gmail.com>
2020-11-10 05:55:00 -0500

    Listen for actions in saga

--- src/ducks/todos/sagas.js
+++ src/ducks/todos/sagas.js
@@ -2,9 +2,9 @@ import { call, takeLatest } from "redux-saga/effects";
 import todoDuck from ".";
 
 function* logActionSaga(action) {
-  yield call(console.log, `${DUCK_NAME}.saga.logAction::`, action);
+  yield call(console.log, `${todoDuck.name}.saga.logAction::`, action);
 }
 
 export default function* sagaWatcher() {
-  yield takeLatest(Object.keys(todoDuck.actions), logActionSaga);
+  yield takeLatest(todoDuck.actionTypes, logActionSaga);
 }
